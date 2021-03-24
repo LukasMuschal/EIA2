ebc82acc2cc042d5caccf79f720fe5070a1eb37f
Lukas Muschal <muschal17@gmail.com>
2021-03-24 14:02:55 +0100

    for-Schleife erstellt

--- A01_RandomPoem/RandomPoem.js
+++ A01_RandomPoem/RandomPoem.js
@@ -2,4 +2,9 @@
 let subjekte = ["Fabian", "Sebastian", "Florian", "Max", "Jannis", "Batuhan"];
 let praedikate = ["liebt", "hasst", "zerstört", "schlägt", "studiert", "riecht"];
 let objekte = ["Tomaten", "Fallen", "Bienen", "Notre-Dame", "seinen Körper", "Tiere"];
+console.log(subjekte, praedikate, objekte);
+var subjekteTotal = 5;
+for (var i = 5; i >= 1; i--) {
+    console.log(i);
+}
 //# sourceMappingURL=RandomPoem.js.map
--- A01_RandomPoem/RandomPoem.ts
+++ A01_RandomPoem/RandomPoem.ts
@@ -3,3 +3,10 @@ let subjekte: string[] = ["Fabian", "Sebastian", "Florian", "Max", "Jannis", "Ba
 let praedikate: string[] = ["liebt", "hasst", "zerstört", "schlägt", "studiert", "riecht"];
 
 let objekte: string[] = ["Tomaten", "Fallen", "Bienen", "Notre-Dame", "seinen Körper", "Tiere"];
+
+console.log(subjekte, praedikate, objekte);
+
+var subjekteTotal: number = 5;
+for (var i: number = 5; i >= 1; i--) {
+    console.log(i);   
+}
