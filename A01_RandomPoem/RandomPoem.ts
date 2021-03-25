948afcab9f45a53a18e7d82edc3c2c7dd620101b
Lukas Muschal <muschal17@gmail.com>
2021-03-25 12:23:18 +0100

    Aufgabe 1 verlibkung

--- A01_RandomPoem/RandomPoem.ts
+++ A01_RandomPoem/RandomPoem.ts
@@ -4,13 +4,20 @@ let praedikate: string[] = ["liebt", "hasst", "zerstört", "schlägt", "studiert
 
 let objekte: string[] = ["Tomaten", "Fallen", "sein Bett", "Notre-Dame", "seinen Körper", "Tiere"];
 
-console.log(subjekte, praedikate, objekte);
+//console.log(subjekte, praedikate, objekte);
 
 
 function getVerse (_subjekte: string[], _praedikate: string[], _objekte: string[]): string {
     let result: string = "";
     let zSubjekte: number = Math.floor(Math.random() * _subjekte.length);
     result += _subjekte.splice(zSubjekte, 1) + " ";
+
+    let zPraedikate: number = Math.floor(Math.random() * _praedikate.length);
+    result += _praedikate.splice(zPraedikate, 1) + " ";
+
+    let zObjekte: number = Math.floor(Math.random() * _objekte.length);
+    result += _objekte.splice(zObjekte, 1) + " ";
+   
     console.log(result);
     return result;
     
@@ -20,5 +27,6 @@ for (var i: number = 6; i <= subjekte.length; i--) {
     if (i == 0) {
         break;
     }
-    console.log(i);   
+    //console.log(i); 
+    getVerse(subjekte, praedikate, objekte)  ;
 }
