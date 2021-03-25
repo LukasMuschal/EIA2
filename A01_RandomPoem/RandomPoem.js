4f967daeeba96ffb65e20269bdf07808e5c10db3
Lukas Muschal <muschal17@gmail.com>
2021-03-25 10:29:53 +0100

    function getVerse greift auf Arry zu

--- A01_RandomPoem/RandomPoem.js
+++ A01_RandomPoem/RandomPoem.js
@@ -1,10 +1,19 @@
 "use strict";
 let subjekte = ["Fabian", "Sebastian", "Florian", "Max", "Jannis", "Batuhan"];
 let praedikate = ["liebt", "hasst", "zerstört", "schlägt", "studiert", "riecht"];
-let objekte = ["Tomaten", "Fallen", "Bienen", "Notre-Dame", "seinen Körper", "Tiere"];
+let objekte = ["Tomaten", "Fallen", "sein Bett", "Notre-Dame", "seinen Körper", "Tiere"];
 console.log(subjekte, praedikate, objekte);
-var subjekteTotal = 5;
-for (var i = 5; i >= 1; i--) {
+function getVerse(_subjekte, _praedikate, _objekte) {
+    let result = "";
+    let zSubjekte = Math.floor(Math.random() * _subjekte.length);
+    result += _subjekte.splice(zSubjekte, 1) + " ";
+    console.log(result);
+    return result;
+}
+for (var i = 6; i <= subjekte.length; i--) {
+    if (i == 0) {
+        break;
+    }
     console.log(i);
 }
 //# sourceMappingURL=RandomPoem.js.map
--- A01_RandomPoem/RandomPoem.ts
+++ A01_RandomPoem/RandomPoem.ts
@@ -2,11 +2,23 @@ let subjekte: string[] = ["Fabian", "Sebastian", "Florian", "Max", "Jannis", "Ba
 
 let praedikate: string[] = ["liebt", "hasst", "zerstört", "schlägt", "studiert", "riecht"];
 
-let objekte: string[] = ["Tomaten", "Fallen", "Bienen", "Notre-Dame", "seinen Körper", "Tiere"];
+let objekte: string[] = ["Tomaten", "Fallen", "sein Bett", "Notre-Dame", "seinen Körper", "Tiere"];
 
 console.log(subjekte, praedikate, objekte);
 
-var subjekteTotal: number = 5;
-for (var i: number = 5; i >= 1; i--) {
+
+function getVerse (_subjekte: string[], _praedikate: string[], _objekte: string[]): string {
+    let result: string = "";
+    let zSubjekte: number = Math.floor(Math.random() * _subjekte.length);
+    result += _subjekte.splice(zSubjekte, 1) + " ";
+    console.log(result);
+    return result;
+    
+}
+
+for (var i: number = 6; i <= subjekte.length; i--) {
+    if (i == 0) {
+        break;
+    }
     console.log(i);   
 }
