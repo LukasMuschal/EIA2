19d4a4242f845ef3e5864e38b831638220d84c71
Lukas Muschal <muschal17@gmail.com>
2021-04-16 22:10:03 +0200

    fertig soweit

--- A02_EventInspector/EventInspector.js
+++ A02_EventInspector/EventInspector.js
@@ -1,31 +1,28 @@
+"use strict";
 window.addEventListener("load", handleLoad);
-
-function handleLoad(_event: Event): void {
+function handleLoad(_event) {
     document.addEventListener("mousemove", setInfoBox);
     document.addEventListener("click", logInfo);
     document.addEventListener("keyup", logInfo);
 }
-
-function setInfoBox(_event: MouseEvent): void {
-    let x: number = _event.clientX;
-    let y: number = _event.clientY;
-    var span: HTMLElement = <HTMLElement> document.querySelector("span");
-
-    let position: HTMLElement = span;
+function setInfoBox(_event) {
+    let x = _event.clientX;
+    let y = _event.clientY;
+    var span = document.querySelector("span");
+    let position = span;
     position.style.left = x + "px";
     position.style.top = y + "px";
     position.textContent = "Koordinaten der Maus(px): " + "  x-Koordinate = " + x + "px" + "  y-Koordinate = " + y + "px"
-    + _event.target;
+        + _event.target;
 }
-
-function logInfo (_event: Event): void {
-    let eventsType: string = _event.type;
-    let eventsTarget: HTMLElement = <HTMLElement>_event.target;
-    let currentTarget: HTMLElement = <HTMLElement>_event.currentTarget;
-    let wholeEvent: Event = <Event>_event;
+function logInfo(_event) {
+    let eventsType = _event.type;
+    let eventsTarget = _event.target;
+    let currentTarget = _event.currentTarget;
+    let wholeEvent = _event;
     console.log("Log Events-Type:" + _event.type);
     console.log("Log Events-Target" + eventsTarget);
     console.log("Curent Target:" + currentTarget);
     console.log("Whole Event" + wholeEvent);
-    
 }
+//# sourceMappingURL=EventInspector.js.map
