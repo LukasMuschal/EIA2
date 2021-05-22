1344a81528555b07d127d640461b4a3a52b22dbc
Lukas Muschal <muschal17@gmail.com>
2021-05-22 15:20:30 +0200

    Old Mac Donald Farm

---
+++ L05_Farm/farm.js
@@ -0,0 +1,39 @@
+"use strict";
+var Donaldsfarm;
+(function (Donaldsfarm) {
+    window.addEventListener("load", load);
+    function load() {
+        class Animal {
+            constructor(_name, _sound, _food, _foodAmount, _foodEaten) {
+                this.name = _name;
+                this.sound = _sound;
+                this.food = _food;
+                this.foodAmount = _foodAmount;
+                this.foodEaten = _foodEaten;
+            }
+            text() {
+                console.log("Hey I am a " + this.name);
+                console.log("Old Mac Donald had a farm E-I-E-I-O And on his farm he had some " + this.name + "s E-I-E-I-O With a " + this.sound + "-" + this.sound + " here and a " + this.sound + "-" + this.sound + " there Here a " + this.sound + " , there a " + this.sound + " Everywhere a " + this.sound);
+            }
+            eat() {
+                console.log(this.name + "s Ate today: " + this.foodEaten + " Kg " + "of " + this.food + "  Old Mac Donald have ", this.foodAmount - this.foodEaten + " Kg" + " left");
+            }
+        }
+        let cow = new Animal("Cow", "MUUH", "Weed", 200, 50);
+        console.log(cow.text());
+        console.log(cow.eat());
+        let horse = new Animal("Horse", "PPRRR", "Apple", 50, 15);
+        console.log(horse.text());
+        console.log(horse.eat());
+        let chicken = new Animal("Chicken", "BogBog", "Corn", 100, 25);
+        console.log(chicken.text());
+        console.log(chicken.eat());
+        let dog = new Animal("Dog", "Wuff", "Meat", 20, 5);
+        console.log(dog.text());
+        console.log(dog.eat());
+        let sheep = new Animal("Sheep", "Määäh", "Straw", 200, 80);
+        console.log(sheep.text());
+        console.log(sheep.eat());
+    }
+})(Donaldsfarm || (Donaldsfarm = {}));
+//# sourceMappingURL=farm.js.map
